c6707f55a0cbd7f54f8b3c629277b2d5f63450f4
tayloraj10 <taylor.alan.johnson@gmail.com>
2022-02-06 01:29:25 -0500

    get basic game working

--- src/components/board.js
+++ src/components/board.js
@@ -1,46 +1,136 @@
 import React from 'react';
 import LetterRow from './letterRow';
 import '../styles/master.css';
+import Button from 'react-bootstrap/Button';
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       words: [],
+      colors: [],
       currentRow: 0,
       currentIndex: 0,
+      correctWord: 'hello',
+      badLetters: [],
+      statusMessage: '',
     };
+    this.myInputRef = React.createRef();
+  }
+
+  onChange(event) {
+    let value = event.target.value;
+    if (value !== '' && value !== ' ' && this.state.currentIndex < 4) {
+      const newCurrentIndex = this.state.currentIndex + 1;
+      this.setState({
+        currentIndex: newCurrentIndex,
+      });
+    }
+    if (this.state.currentIndex === 4) {
+    }
+  }
+
+  onKeyDown(event) {
+    if (event.keyCode === 8 && this.state.currentIndex > 0) {
+      const newCurrentIndex = this.state.currentIndex - 1;
+      this.setState({
+        currentIndex: newCurrentIndex,
+      });
+    }
+  }
+
+  onSubmit() {
+    let colors = [];
+    let newBadLetters = this.state.badLetters;
+    let word = this.state.words[this.state.currentRow];
+
+    for (let i = 0; i < word.length; i++) {
+      if (this.state.correctWord[i] === word[i]) {
+        colors.push('green');
+      } else if (this.state.correctWord.includes(word[i])) {
+        colors.push('yellow');
+      } else {
+        colors.push('');
+        if (!this.state.badLetters.includes(word[i])) {
+          newBadLetters.push(word[i]);
+        }
+      }
+    }
+
+    let newColors = this.state.colors;
+    newColors.push(colors);
+
+    if (word === this.state.correctWord) {
+      this.setState({
+        statusMessage: 'Congrats, you got the word!',
+      });
+      return;
+    }
+
+    let newRow = this.state.currentRow + 1;
+    this.setState(
+      {
+        currentRow: newRow,
+        currentIndex: 0,
+        colors: newColors,
+        badLetters: newBadLetters,
+      },
+      () => {
+        this.myInputRef.current.focus();
+      }
+    );
+  }
+
+  wordChange(word) {
+    let newWords = this.state.words;
+    newWords[this.state.currentRow] = word;
+    this.setState({
+      words: newWords,
+    });
+  }
+
+  renderRow(i) {
+    return (
+      <LetterRow
+        currentIndex={this.state.currentIndex}
+        currentRow={this.state.currentRow}
+        rowIndex={i}
+        onChange={(value) => this.onChange(value)}
+        onKeyDown={(value) => this.onKeyDown(value)}
+        refDown={this.state.currentRow === i ? this.myInputRef : null}
+        wordChange={(word) => this.wordChange(word)}
+        colors={this.state.colors[i] === undefined ? [] : this.state.colors[i]}
+      />
+    );
   }
 
   render() {
     return (
       <div className='board'>
-        <LetterRow
-          currentIndex={this.state.currentIndex}
-          currentRow={this.state.currentRow}
-          rowIndex={0}
-        />
-        <LetterRow
-          currentIndex={this.state.currentIndex}
-          currentRow={this.state.currentRow}
-          rowIndex={1}
-        />
-        <LetterRow
-          currentIndex={this.state.currentIndex}
-          currentRow={this.state.currentRow}
-          rowIndex={2}
-        />
-        <LetterRow
-          currentIndex={this.state.currentIndex}
-          currentRow={this.state.currentRow}
-          rowIndex={3}
-        />
-        <LetterRow
-          currentIndex={this.state.currentIndex}
-          currentRow={this.state.currentRow}
-          rowIndex={4}
-        />
-        <LetterRow currentIndex={this.state.currentIndex} />
+        <h3 className='status blink_me'>{this.state.statusMessage}</h3>
+        {this.renderRow(0)}
+        {this.renderRow(1)}
+        {this.renderRow(2)}
+        {this.renderRow(3)}
+        {this.renderRow(4)}
+        {this.renderRow(5)}
+        {this.state.currentIndex === 4 &&
+        this.state.words[this.state.currentRow].length === 5 ? (
+          <Button
+            variant='secondary'
+            className='button'
+            size='lg'
+            onClick={() => this.onSubmit()}
+          >
+            Submit
+          </Button>
+        ) : null}
+        <h4 className='badLetters'>Letters not in word</h4>
+        <ul>
+          {this.state.badLetters.map((letter) => {
+            return <li key={letter}>{letter + ', '}</li>;
+          })}
+        </ul>
       </div>
     );
   }
--- src/components/letterRow.js
+++ src/components/letterRow.js
@@ -2,6 +2,13 @@ import React from 'react';
 import '../styles/master.css';
 
 class LetterRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: '',
+    };
+  }
+
   isSquareEnabled(index) {
     return this.props.currentIndex === index ? '' : 'disabled';
   }
@@ -9,6 +16,58 @@ class LetterRow extends React.Component {
   isRowEnabled(index) {
     return this.props.currentRow === index ? '' : 'disabled';
   }
+
+  onChange = (e) => {
+    this.props.onChange(e);
+    if (this.props.currentIndex < 4) {
+      setTimeout(function () {
+        e.target.nextElementSibling.focus();
+      });
+    }
+
+    let newWord;
+    if (
+      this.state.word.length <= 5 &&
+      this.props.currentIndex === this.state.word.length
+    ) {
+      newWord = this.state.word + e.target.value;
+    } else {
+      let chars = this.state.word.split('');
+      chars[this.props.currentIndex] = e.target.value;
+      newWord = chars.join('');
+    }
+
+    this.setState(
+      {
+        word: newWord,
+      },
+      () => {
+        this.props.wordChange(this.state.word);
+      }
+    );
+  };
+
+  onKeyDown = (e) => {
+    if (this.props.currentIndex > 0) {
+      this.props.onKeyDown(e);
+      e.target.value = '';
+      setTimeout(function () {
+        e.target.previousElementSibling.focus();
+      });
+    }
+    if (e.keyCode === 8) {
+      let newWord = this.state.word.slice(0, -1);
+
+      this.setState(
+        {
+          word: newWord,
+        },
+        () => {
+          this.props.wordChange(this.state.word);
+        }
+      );
+    }
+  };
 
   renderSquare(i) {
     return (
@@ -18,22 +77,46 @@ class LetterRow extends React.Component {
         className='square'
         disabled={this.isSquareEnabled(i)}
         maxLength={1}
+        onChange={this.onChange}
+        onKeyDown={this.onKeyDown}
+        autoFocus={i === 0 && this.props.currentRow === this.props.rowIndex}
+        ref={this.props.currentIndex === i ? this.props.refDown : null}
+        style={{
+          border:
+            this.props.colors === undefined
+              ? '3px solid #3a3a3c'
+              : getBorderColor(this.props.colors[i]),
+        }}
       />
     );
   }
 
+  componentDidUpdate() {}
+
   render() {
     return (
       <div>
-        <fieldset disabled={this.isRowEnabled(this.props.rowIndex)}>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-        </fieldset>
+        <form>
+          <fieldset disabled={this.isRowEnabled(this.props.rowIndex)}>
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+          </fieldset>
+        </form>
       </div>
     );
+  }
+}
+
+function getBorderColor(color) {
+  if (color === 'green') {
+    return '3px solid green';
+  } else if (color === 'yellow') {
+    return '3px solid yellow';
+  } else {
+    return '3px solid #3a3a3c';
   }
 }
 
